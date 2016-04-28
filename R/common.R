665f6eb6a62a23c5a46e8c2e49b2ab0d1a3ea70c
Dalevi, Daniel <Daniel.Dalevi@astrazeneca.com>
2016-04-28 18:10:48 +0200

    one extra change

--- R/common.R
+++ R/common.R
@@ -12,7 +12,9 @@ splineResolution <- function() {
 ##' @param x EventModel object
 ##' @param y EventModelExtended object
 checkValidTimeCut <- function( x, y ) {
-  if( y@time.cut < max(x@event.data@subject.data$time, na.rm=T) ) {
-    warning( "The change point is less than the largest time in the right censored model!" )
+  if( ( y@time.cut - max(x@event.data@subject.data$time, na.rm=T) ) > 2 ) {
+    warning( paste0( "The change point should equal the largest time in the right censored model!", 
+                     "That or all patients have had events! Regardless, this situation may result", 
+                     "undefined behaiviour!" ) )
   } 
 }
